d8435dd88c8cad7cd674fe21cf26b2f8f70a13a8
Roman Ventskus <scofield665@gmail.com>
2016-05-13 21:12:54 +0300

    Remove web

--- src/main/webapp/app/app.js
+++
@@ -1,4 +0,0 @@
-angular.module('hello', ['ngRoute', 'ui.bootstrap', 'ui.bootstrap.tpls', 'app-controllers'])
-    .config(function($routeProvider, $httpProvider) {
-        $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-    });
